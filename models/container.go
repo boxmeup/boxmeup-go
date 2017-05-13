74c6809f8b0a7378d11b82885cb5ce4842795e44
Chris Saylor <cjsaylor@gmail.com>
2017-05-13 10:20:30 -0400

    Add method to get container by ID.

--- models/container.go
+++ models/container.go
@@ -5,12 +5,13 @@ import "database/sql"
 
 // Container represents an individual container that will contain items.
 type Container struct {
-	ID       int64     `json:"id,omitempty"`
-	User     User      `json:"-,omitempty"`
-	Name     string    `json:"name,omitempty"`
-	Location Location  `json:"location,omitempty"`
-	Created  time.Time `json:"created,omitempty"`
-	Modified time.Time `json:"modified,omitempty"`
+	ID       int64     `json:"id"`
+	User     User      `json:"-"`
+	Name     string    `json:"name"`
+	UUID     string    `json:"uuid"`
+	Location Location  `json:"-"` // @todo update when location is implemented
+	Created  time.Time `json:"created"`
+	Modified time.Time `json:"modified"`
 }
 
 // CreateContainer persists a container to the database
@@ -24,4 +25,23 @@ func CreateContainer(db *sql.DB, container *Container) error {
 	container.ID, _ = res.LastInsertId()
 
 	return err
+}
+
+// ContainerbyID retrieves a container by its primary ID
+func ContainerbyID(db *sql.DB, ID int64) (Container, error) {
+	var userID int64
+	var locationID int64
+	q := `
+		select id, user_id, location_id, name, uuid, created, modified
+		from containers
+		where id = ?
+	`
+	var container Container
+	err := db.QueryRow(q, ID).Scan(&container.ID, &userID, &locationID, &container.Name, &container.UUID, &container.Created, &container.Modified)
+	if err != nil {
+		return container, err
+	}
+	container.User, err = GetUserByID(db, userID)
+
+	return container, err
 }
