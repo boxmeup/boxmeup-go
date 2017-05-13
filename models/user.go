357f81572b0da21568851d40e6bf48923aa8929d
Chris Saylor <cjsaylor@gmail.com>
2017-05-13 10:20:30 -0400

    Add authentication mechanism that returns a JWT.

--- models/user.go
+++ models/user.go
@@ -2,13 +2,18 @@ package models
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"time"
+
+	"crypto/sha1"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 // User is a user entity structure
 type User struct {
-	ID            int       `json:"id"`
+	ID            int64     `json:"id"`
 	Email         string    `json:"email"`
 	Password      string    `json:"-"`
 	UUID          string    `json:"uuid"`
@@ -16,6 +21,41 @@ type User struct {
 	ResetPassword bool      `json:"-"`
 	Created       time.Time `json:"created"`
 	Modified      time.Time `json:"modified"`
+}
+
+type AuthConfig struct {
+	LegacySalt string
+	JWTSecret  string
+}
+
+func hashPassword(config AuthConfig, password string) string {
+	data := []byte(fmt.Sprintf("%v%v", config.LegacySalt, password))
+	return fmt.Sprintf("%x", sha1.Sum(data))
+}
+
+// Login authenticates user credentials and produces a signed JWT
+func Login(db *sql.DB, config AuthConfig, email string, password string) (string, error) {
+	hashedPassword := hashPassword(config, password)
+	var ID int
+	var UUID string
+	q := `
+		select id, uuid from users where email = ? and password = ?
+	`
+	err := db.QueryRow(q, email, hashedPassword).Scan(&ID, &UUID)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			log.Fatal(err)
+		}
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":   ID,
+		"uuid": UUID,
+		// 5 day expiration
+		"nbf": time.Now().AddDate(0, 0, 5).Unix(),
+	})
+	return token.SignedString([]byte(config.JWTSecret))
 }
 
 // GetUserByID resolves with a user on the channel.
