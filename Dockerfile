d708cd7f24fb0661ca403a7a036b09d0aea4fb58
Chris Saylor <cjsaylor@gmail.com>
2017-05-18 19:46:09 -0400

    Dockerize the app.

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM golang:1.8.1 as builder
+COPY . /go/src/github.com/cjsaylor/boxmeup-go
+WORKDIR /go/src/github.com/cjsaylor/boxmeup-go
+RUN GOOS=linux GOARCH=386 go build -v
+
+FROM alpine:latest
+RUN apk --no-cache add ca-certificates
+RUN adduser -D -u 1000 appuser
+USER appuser
+WORKDIR /app
+COPY --from=builder /go/src/github.com/cjsaylor/boxmeup-go/boxmeup-go app
+EXPOSE 8080
+
+CMD ["./app"]
--- config.go
+++ config.go
@@ -9,7 +9,7 @@ import (
 // Config is a placeholder for available configurations
 type Config struct {
 	Port       int    `env:"PORT" envDefault:"8080"`
-	MysqlDSN   string `env:"MYSQL_DSN" envDefault:"guest:guest@tcp(guest:3306)/bmu"`
+	MysqlDSN   string `env:"MYSQL_DSN" envDefault:"boxmeup:boxmeup@tcp(localhost:3306)/boxmeup"`
 	LegacySalt string `env:"LEGACY_SALT,required"`
 	JWTSecret  string `env:"JWT_SECRET,required"`
 	WebHost    string `env:"WEB_HOST" envDefault:"http://localhost:8080"`
